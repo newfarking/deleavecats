18c305ab09f9efaad0a4833de3d3bcbf610216e3
newfarking <newfarking@newfarkingdeMacBook-Pro.local>
2014-08-10 18:01:20 +0800

    Initial Commit

---
+++ deleavecats/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  deleavecats
+//
+//  Created by newfarking on 14-8-10.
+//  Copyright newfarking 2014年. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ deleavecats/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  deleavecats
+//
+//  Created by newfarking on 14-8-10.
+//  Copyright newfarking 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
